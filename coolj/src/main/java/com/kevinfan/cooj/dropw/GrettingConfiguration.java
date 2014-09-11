3a9a065334e285488bc05ed1d6de6ea5aa360105
kevinjom0718 <gokevinvan@gmail.com>
2014-09-11 17:05:14 +0800

    remove unsed files

--- coolj/src/main/java/com/kevinfan/cooj/dropw/GrettingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.kevinfan.cooj.dropw;
-
-import io.dropwizard.Configuration;
-
-/**
- * Created by kevinjom on 14-8-28.
- */
-public class GrettingConfiguration extends Configuration {
-}
